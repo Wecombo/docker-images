915e70c86f9584090811266fe82205793ade4893
Wecombo <wecombo@qq.com>
2024-05-10 06:45:24 +0000

    Update repository used in download script.

--- download.sh
+++ download.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
+set -ex
+
 image=${1}
 tmp_image=${image#*\/}
 gen_image=${tmp_image//[\/:]/-}
 
-echo "will download this image from: registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}"
-docker pull registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}
-docker tag registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image} ${image}
-docker rmi registry.cn-hangzhou.aliyuncs.com/soolaugust/image:${gen_image}
+echo "will download this image from: registry.cn-hangzhou.aliyuncs.com/resolver/docker-images:${gen_image}"
+docker pull registry.cn-hangzhou.aliyuncs.com/resolver/docker-images:${gen_image}
+docker tag registry.cn-hangzhou.aliyuncs.com/resolver/docker-images:${gen_image} ${image}
+docker rmi registry.cn-hangzhou.aliyuncs.com/resolver/docker-images:${gen_image}
 echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> download successfully!\n"
